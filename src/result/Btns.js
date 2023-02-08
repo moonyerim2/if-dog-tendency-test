5b24ec6525353d7f0e1b0527b98d6b5af0bb9b5e
jaeupsu <hyeongs2323@gmail.com>
2023-02-08 22:11:09 +0900

    feat: result btns before design add

---
+++ src/result/Btns.js
@@ -0,0 +1,15 @@
+import ShareBtn from "./ShareBtn";
+import RetryBtn from "./RetryBtn";
+import OzCtaBtns from "./OzCtaBtns";
+
+function Btns() {
+  return (
+    <div>
+      <ShareBtn />
+      <RetryBtn />
+      <OzCtaBtns />
+    </div>
+  );
+}
+
+export default Btns;
