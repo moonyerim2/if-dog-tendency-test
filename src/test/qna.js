450b0c3010156387b3fe84188a2882201cc83133
moonyerim2 <moonyerim2@gmail.com>
2023-02-08 22:00:23 +0900

    feat: 문항 데이터 추가

---
+++ src/test/qna.js
@@ -0,0 +1,165 @@
+export const qnaList = [
+  {
+    q: "1. 친구 집에 갔는데 배가 고프다",
+    a: [
+      { answer: "1. 밥먹자고 물으면 “그래!”", type: ["ENTJ", "ESFJ"] },
+      { answer: "2. 밥먹자고 물으면 “아냐 괜찮아”", type: ["ISFJ", "ISTJ"] },
+      { answer: "3. 내가 먼저 “배고프다 밥먹자!”", type: ["INTP", "ENFP"] },
+    ],
+  },
+  {
+    q: "2. 무인도에 고립됐다.",
+    a: [
+      {
+        answer: "1. 부지런히 코코넛을 주워 모은다.",
+        type: ["ISTJ", "INTP", "ENTJ"],
+      },
+      { answer: "2. 루아루(하와이식 파티)를 연다.", type: ["ESFJ", "ENFP"] },
+      {
+        answer: "3. 사람들을 감시하고 백사장에 SOS를 만든다.",
+        type: ["ISFJ"],
+      },
+    ],
+  },
+  {
+    q: "3. 아침이 되었다.",
+    a: [
+      {
+        answer: "1. 아직 꿈나라.",
+        type: ["INTP"],
+      },
+      { answer: "2. SNS 확인으로 하루를 시작한다. ", type: ["ENFP"] },
+      {
+        answer: "3. 그래놀라를 먹고 아침 6시에 조깅하러 간다.",
+        type: ["ISTJ", "ENTJ", "ESFJ", "ISFJ"],
+      },
+    ],
+  },
+  {
+    q: "4. 수업시간이 한창일 때 나는",
+    a: [
+      {
+        answer: "1. 노트필기를 하고, 수업에 집중한다.",
+        type: ["ISTJ", "ENTJ", "ESFJ", "ISFJ"],
+      },
+      { answer: "2. 잠이나 자야지.", type: ["INTP"] },
+      {
+        answer: "3. 친구들과 농담한다.",
+        type: ["ENFP"],
+      },
+    ],
+  },
+  {
+    q: "5. 나는 세상을",
+    a: [
+      {
+        answer: "1. 지배하고 말테야.",
+        type: ["ENTJ", "ESFJ"],
+      },
+      { answer: "2. 구원하고 말테야.", type: ["ISTJ"] },
+      {
+        answer: "3. 탈출하고 말테야.",
+        type: ["ISFJ", "INTP", "ENFP"],
+      },
+    ],
+  },
+  {
+    q: "6. 누가 자꾸 쳐다본다.",
+    a: [
+      {
+        answer: "1. 뭐 필요하세요?",
+        type: ["ESFJ", "ENFP"],
+      },
+      { answer: "2. 당황스럽지만 웃어준다.", type: ["ISFJ"] },
+      {
+        answer: "3. 딴데 본다.",
+        type: ["INTP"],
+      },
+      {
+        answer: "4. 똑같이 쳐다본다.",
+        type: ["ENTJ", "ISTJ"],
+      },
+    ],
+  },
+  {
+    q: "7. 밤에 길을 걷다 문득 위를 올려다 보았다.",
+    a: [
+      {
+        answer: "1. 별 좀 봐",
+        type: ["ISTJ", "INTP"],
+      },
+      { answer: "2. 달 좀 봐", type: ["ENTJ", "ESFJ"] },
+      {
+        answer: "3. 하늘 좀 봐.",
+        type: ["ISFJ", "ENFP"],
+      },
+    ],
+  },
+  {
+    q: "8. 겨울에 만났을때",
+    a: [
+      {
+        answer: "1. 옷*100",
+        type: ["ISFJ", "INTP"],
+      },
+      { answer: "2. 겨울 싫어", type: ["ENTJ", "ISTJ"] },
+      {
+        answer: "3. 기분전환으로 화려한 옷 입기",
+        type: ["ESFJ", "ENFP"],
+      },
+    ],
+  },
+  {
+    q: "9. 무언가에 방해 받고 있을때",
+    a: [
+      {
+        answer: "1. 신경쓰지 않는다.",
+        type: ["ISFJ", "ESFJ"],
+      },
+      { answer: "2. 조금만 조용히 해줘", type: ["ENFP"] },
+      {
+        answer: "3. 조용히해",
+        type: ["ISTJ", "ENTJ"],
+      },
+      {
+        answer: "4. 싸늘하게 말한다. 물어버린다.",
+        type: ["INTP"],
+      },
+    ],
+  },
+  {
+    q: "10. 인생에 관한 견해",
+    a: [
+      {
+        answer: "1. 인생은 게임",
+        type: ["ESFJ", "ENFP", "ENTJ"],
+      },
+      { answer: "2. 무슨 얘기하는 중?", type: ["INTP"] },
+      {
+        answer: "3. 생각하지 않을래",
+        type: ["ISFJ", "ISTJ"],
+      },
+    ],
+  },
+  {
+    q: "11. 색깔로 보는 나",
+    a: [
+      {
+        answer: "1. 노란색 (빛, 지성)",
+        type: ["ENFP"],
+      },
+      {
+        answer: "2. 녹색 (균형, 조화, 사랑, 소통, 자연)",
+        type: ["ISFJ", "ISTJ"],
+      },
+      {
+        answer: "3. 빨간색 (활력, 열정, 자발성)",
+        type: ["ESFJ"],
+      },
+      {
+        answer: "4. 검정 (교양, 권력, 논리)",
+        type: ["INTP", "ENTJ"],
+      },
+    ],
+  },
+];
