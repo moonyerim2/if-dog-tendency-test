ab232c5430803a42fee90fdfdbf03a1d2a2f731e
moonyerim2 <moonyerim2@gmail.com>
2023-02-09 18:39:09 +0900

    rename: Home -> HomePage 컴포넌트 네이밍 변경

---
+++ src/home/HomePage.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const HomePage = () => {
+  const startClick = (event) => {
+    // move page2
+    console.log("move page 2");
+  };
+  return (
+    <div className="container">
+      <header>
+        <h2>Hot 도그</h2>
+      </header>
+      <section>
+        <h3>내가 개라면?</h3>
+        {/* <img alt= insert_img> */}
+        <p>
+          Hot 도그를 먹은 당신,, 다음날 눈을 떠보니 강아지가 되어있었다..!!
+          과연-? 나는 어떤 강아지 일까?
+        </p>
+      </section>
+      <button className="start-btn" onClick={startClick}>
+        {" "}
+        시작하기{" "}
+      </button>
+    </div>
+  );
+};
+
+export default HomePage;
