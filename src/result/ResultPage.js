eec9d954118abdc252e7fc7d3dde9de90c01f8ad
moonyerim2 <moonyerim2@gmail.com>
2023-02-09 19:28:40 +0900

    feat: context에서 result 불러오기 추가

--- src/result/ResultPage.js
+++ src/result/ResultPage.js
@@ -1,12 +1,17 @@
+import React, { useContext } from "react";
 import OtherResults from "./OtherResults";
 import Result from "./Result";
 import Btns from "./Btns";
 
 import Dogs from "../data/dogsDB";
+import { ResultContext } from "../test/ResultProvider";
 
 const dogs = new Dogs();
 
 function ResultPage() {
+  //정상적으로 테스트가 끝나면 result에 강아지 종류가 담겨있습니다.
+  const { result } = useContext(ResultContext);
+
   return (
     <div>
       <Result dogs={dogs} />
