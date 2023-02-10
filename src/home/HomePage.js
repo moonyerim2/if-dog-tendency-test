10d7d58ab21ae21c4574c1edbe2b426c2bada5d5
moonyerim2 <moonyerim2@gmail.com>
2023-02-10 17:45:46 +0900

    feat: StartPage UI 추가 - 페이지 배경 추가

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,5 +1,20 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const TitleImg = styled.img`
+  display: block;
+  margin: 120px auto 0;
+`;
+
+const DescriptionImg = styled.img`
+  display: block;
+  margin: 64px auto 0;
+`;
+
+const StartButtonImg = styled.img`
+  display: block;
+`;
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -11,20 +26,49 @@ const HomePage = () => {
   return (
     <div className="container">
       <header>
-        <h2>Hot 도그</h2>
+        <h2 style={{ color: "transparent" }}>Hot 도그</h2>
+        <TitleImg
+          src={`${process.env.PUBLIC_URL}/header.png`}
+          alt="테스트 제목"
+        />
       </header>
       <section>
-        <h3>내가 개라면?</h3>
         {/* <img alt= insert_img> */}
-        <p>
+        <DescriptionImg
+          src={`${process.env.PUBLIC_URL}/description.png`}
+          alt="테스트 설명"
+        />
+        <p style={{ color: "transparent" }}>
           Hot 도그를 먹은 당신,, 다음날 눈을 떠보니 강아지가 되어있었다..!!
           과연-? 나는 어떤 강아지 일까?
         </p>
       </section>
-      <button className="start-btn" onClick={startClick}>
-        {" "}
-        시작하기{" "}
-      </button>
+      <div
+        style={{
+          position: "relative",
+          width: "320px",
+          height: "80px",
+          margin: "0 auto",
+        }}
+      >
+        <button
+          className="start-btn"
+          onClick={startClick}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "320px",
+            height: "80px",
+            backgroundColor: "transparent",
+            color: "transparent",
+            border: "none",
+          }}
+        >
+          Start
+        </button>
+        <StartButtonImg src={`${process.env.PUBLIC_URL}/start-button.png`} />
+      </div>
     </div>
   );
 };
