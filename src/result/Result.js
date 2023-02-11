21781512bd26cd3bab7c394ae17b123bc5c40376
jaeupsu <hyeongs2323@gmail.com>
2023-02-11 18:08:30 +0900

    feat: result update

--- src/result/Result.js
+++ src/result/Result.js
@@ -1,34 +1,17 @@
 import { useState, useEffect } from "react";
+import OtherResults from "./OtherResults";
 
-function Result({ dogs }) {
+function Result({ dogs, species }) {
   const [result, setResult] = useState({});
   const [total, setTotal] = useState(0);
-  const [ratios, setRatios] = useState([]);
-  const [colDatas, setColDatas] = useState([]);
-
-  const [dataByMbti, setDataByMbti] = useState("");
-  const [dataById, setDataById] = useState("");
-
-  const dog = {
-    name: "시베리안 허스키",
-    id: 5,
-    mbti: "ISTJ",
-    contents:
-      "대형견인 허스키는 덩치만 컸지 순둥이랍니다. 아이들과 다른 작은 동물들과도 잘 지내죠. 하지만 독립적인 성격이랑 혼자인 시간도 필요해요!",
-    img: "이미지",
-    counts: 1,
-  };
 
   useEffect(() => {
-    dogs.get(setResult, dog.name);
+    dogs.update(species);
+    dogs.get(setResult, species);
   }, []);
 
   useEffect(() => {
     dogs.getParticipants(setTotal);
-    dogs.getRatios(setRatios, total);
-    dogs.getFieldValues(setColDatas, "name");
-    dogs.getFieldValueByMbti(setDataByMbti, "ISFJ", "contents");
-    dogs.getFieldValueById(setDataById, "1", "name");
   }, [total]);
 
   return (
@@ -39,7 +22,7 @@ function Result({ dogs }) {
       <br />
       <span>
         <div key={result.id}>
-          <span>{`당신이 개라면 ? ${result.name} (${result.mbti})`}</span>
+          <span>{`당신이 개라면 ? ${result.name}  (${result.species})`}</span>
           <br />
           <span>{`${result.contents}`}</span>
         </div>
@@ -49,25 +32,6 @@ function Result({ dogs }) {
       <br />
 
       <span>참여자수 : {total}</span>
-      <br />
-      <br />
-      <br />
-      <ul>
-        {ratios.map((num, i) => {
-          return (
-            <li key={i}>
-              <h1>{colDatas[i]}</h1>
-              {String(num) + "% "}
-              <br />
-              <br />
-            </li>
-          );
-        })}
-      </ul>
-
-      <span>{dataById}</span>
-      <br />
-      <span>{dataByMbti}</span>
     </div>
   );
 }
