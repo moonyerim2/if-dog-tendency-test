91dbac443491d4eb903a9e486e8f89a7568289aa
jaeupsu <hyeongs2323@gmail.com>
2023-02-11 18:11:24 +0900

    feat: ozctabtns add

--- src/result/OzCtaBtns.js
+++ src/result/OzCtaBtns.js
@@ -1,5 +1,18 @@
 function OzCtaBtns() {
-  return <div>CtaBtns</div>;
+  return (
+    <div>
+      <h1>
+        본 심리테스트는 오즈코딩스쿨 린스타트업 맞춤형 Serverless MVP 개발
+        전문가 양성 과정 1기 2팀이 2일 만에 구현한 결과물입니다.
+      </h1>
+      <a href="https://ozcodingschool.com/">
+        <input type="button" value="OZ 코딩스쿨 알아보기"></input>
+      </a>
+      <a href="https://legend-palm-1f1.notion.site/2-_-cce963197d034ae5b0ab4a1bd3530365">
+        <input type="button" value="팀 소개 페이지"></input>
+      </a>
+    </div>
+  );
 }
 
 export default OzCtaBtns;
