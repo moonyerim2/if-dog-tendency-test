4c7f225d24a313727af6e4d2713f83482bd10a06
jaeupsu <hyeongs2323@gmail.com>
2023-02-11 18:27:03 +0900

    feat: Retry Button Add

--- src/result/RetryBtn.js
+++ src/result/RetryBtn.js
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function RetryBtn() {
-  return <div>Retry</div>;
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/");
+  };
+  return <button onClick={goHome}>Retry</button>;
 }
 
 export default RetryBtn;
