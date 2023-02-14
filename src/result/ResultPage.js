f782fa229146de10cc72b7b48d6bd7d9761ef96c
moonyerim2 <moonyerim2@gmail.com>
2023-02-14 15:26:33 +0900

    refactor: 사용하지 않는 코드 삭제

--- src/result/ResultPage.js
+++ src/result/ResultPage.js
@@ -1,29 +1,23 @@
-import React, { useContext, useEffect, useState } from "react";
-import OtherResults from "./OtherResults";
-import Result from "./Result";
-import Btns from "./Btns";
-import PieCharts from "./PieCharts";
+import React, { useContext } from 'react';
+import OtherResults from './OtherResults';
+import Result from './Result';
+import Btns from './Btns';
+import PieCharts from './PieCharts';
 
-import Dogs from "../data/dogsDB";
+import Dogs from '../data/dogsDB';
 
-import { ResultContext } from "../test/ResultProvider";
+import { ResultContext } from '../test/ResultProvider';
 
 const dogs = new Dogs();
 
 function ResultPage() {
   const { result } = useContext(ResultContext);
 
-  const [dog, setDog] = useState({});
-
-  useEffect(() => {
-    dogs.get(setDog, result);
-  }, []);
-
   return (
     <div
       style={{
-        height: "100%",
-        padding: "0 20px",
+        height: '100%',
+        padding: '0 20px',
       }}
     >
       <Result species={result} dogs={dogs} />
