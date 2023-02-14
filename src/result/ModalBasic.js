abdff28033663b3bf066afa5e694ad872f8252df
moonyerim2 <moonyerim2@gmail.com>
2023-02-14 21:28:11 +0900

    feat: 모달창 css 수정

--- src/result/ModalBasic.js
+++ src/result/ModalBasic.js
@@ -1,43 +1,43 @@
-import React from "react";
-import OzCtaBtns from "./OzCtaBtns";
-import styled from "styled-components";
-//="from "./ModalBasic.module.css';
-//import styled from "@emotion/styled";
+import React from 'react';
+import OzCtaBtns from './OzCtaBtns';
+import styled from 'styled-components';
 
-const Wrapper=styled.div`
-    position:absolute;
-    top:50%;
-    left:50%;
-    transform: translate(-50%, 50%);
-    width:330px;
-    height: 170px;
-    background-color:#FFFF;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    border-radius:8px;
+const Wrapper = styled.div`
+  position: fixed;
+  z-index: 999;
+  top: calc(50vh - 25%);
+  left: 50%;
+  transform: translate(-50%, 50%);
+  width: 285px;
+  height: 170px;
+  padding: 10px 10px 20px;
+  background-color: #ffff;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 8px;
 `;
-function ModalBasic({ setModalOpen }){
 
-    const closeModal=()=>{
-        setModalOpen(false);
-        console.log("bye modal");
-    };
+function ModalBasic({ setModalOpen }) {
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
-    return(
-        <Wrapper>
-            <button 
-                className="close" 
-                style={{
-                    position: "absolute",
-                    right: 10,
-                    top:10,
-                    color: "red",
-                    border:"none"
-                }}
-                onClick={closeModal}>
-            X
-            </button>
-            <OzCtaBtns/>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <button
+        className="close"
+        style={{
+          position: 'absolute',
+          right: 10,
+          top: 10,
+          color: 'red',
+          border: 'none',
+        }}
+        onClick={closeModal}
+      >
+        X
+      </button>
+      <OzCtaBtns />
+    </Wrapper>
+  );
 }
 export default ModalBasic;
--- src/result/OzCtaBtns.js
+++ src/result/OzCtaBtns.js
@@ -1,45 +1,43 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React from 'react';
+import styled from '@emotion/styled';
 
 const Container = styled.div`
-  
   margin: 27px;
-  text-align:center;
-  color:black;
-  overflow:hidden;
+  text-align: center;
+  color: black;
+  overflow: hidden;
 `;
+
 const InputBox = styled.div`
-display:flex;
-justify-content:space-between;
-align-items:center;
-
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
-const Input = styled.input`
-    border:none;
-    border-radius:3px;
-    margin-top: 15px;
-    height: 36px;
-    background-color: #FFF7EB;
-    color:black;
-    cursor:pointer;
-    
 
+const Input = styled.input`
+  border: none;
+  border-radius: 5px;
+  margin-top: 20px;
+  height: 40px;
+  background-color: #fff7eb;
+  color: black;
+  cursor: pointer;
 `;
 
 function OzCtaBtns() {
   return (
     <Container>
-      <h1 style={{lineHeight:"18px"}}>
+      <h1 style={{ lineHeight: '20px' }}>
         본 심리테스트는 오즈코딩스쿨 린스타트업 맞춤형 Serverless MVP 개발
         전문가 양성 과정 1기 2팀이 구현한 결과물입니다.
       </h1>
       <InputBox>
-      <a href="https://ozcodingschool.com/">
-        <Input type="button" value="OZ 코딩스쿨 알아보기"></Input>
-      </a>
-      <a href="https://legend-palm-1f1.notion.site/2-_-cce963197d034ae5b0ab4a1bd3530365">
-        <Input type="button" value="팀 소개 페이지"></Input>
-      </a>
+        <a href="https://ozcodingschool.com/">
+          <Input type="button" value="OZ 코딩스쿨 알아보기"></Input>
+        </a>
+        <a href="https://legend-palm-1f1.notion.site/2-_-cce963197d034ae5b0ab4a1bd3530365">
+          <Input type="button" value="팀 소개 페이지"></Input>
+        </a>
       </InputBox>
     </Container>
   );
