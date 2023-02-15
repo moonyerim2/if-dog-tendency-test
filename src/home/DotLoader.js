d16c605fab3da3e49901b3ac9f6080a67d2daff5
moonyerim2 <moonyerim2@gmail.com>
2023-02-15 17:50:46 +0900

    feat: DotLoader 컴포넌트 css 수정

--- src/home/DotLoader.js
+++ src/home/DotLoader.js
@@ -1,97 +1,102 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React from 'react';
+import { motion } from 'framer-motion';
 
-
-const Top={///최상위 컨테이너
-    position:"absolute",
-    display: "flex",
-    top:"20%",
-    left:"50%",
-    transform: "translate(-50%, 5%)",
-}
-const loadingContainer = { //dot을 감싸는 컨테이너 
-    width: "18rem",
-    height: "18rem",
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems:"center",
-};
-const loadingCircle = {//dot 
-    display: "block",
-    width: "2.2rem",
-    height: "2.2rem",
-    backgroundColor: "#fa9d00",
-    border: "4px solid #7B4E25", 
-    borderRadius: "5rem",
-    margin: "10px",
+const Top = {
+  ///최상위 컨테이너
+  position: 'absolute',
+  display: 'flex',
+  top: '47%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
 };
 
-const loadingContainerVariants = { //움직이는 속도 
-    start: {
-        transition: {
-        staggerChildren: 0.5,
-        },
-    },
-    end: {
-        transition: {
-        staggerChildren: 0.5,
-        },
-    },
+const loadingContainer = {
+  //dot을 감싸는 컨테이너
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  width: '9.5rem',
 };
 
-const loadingCircleVariants = {//위에서 아래로 떨어지는 높이 
-    start: {
-        y: "0%",
-    },
-    end: {
-        y: "70%",
-    },
+const loadingCircle = {
+  //dot
+  display: 'block',
+  width: '0.7rem',
+  height: '1.2rem',
+  backgroundColor: '#fa9d00',
+  border: '4px solid #7B4E25',
+  borderRadius: '5rem',
 };
+
+const loadingContainerVariants = {
+  //움직이는 속도
+  start: {
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const loadingCircleVariants = {
+  //위에서 아래로 떨어지는 높이
+  start: {
+    y: '0%',
+  },
+  end: {
+    y: '70%',
+  },
+};
+
 const loadingCircleTransition = {
-    duration : 0.4,
-    yoyo : 'Infinity',
-    ease: 'easeInOut'
-}
+  duration: 0.8,
+  repeat: 'Infinity',
+  ease: 'easeInOut',
+};
 
 const DotLoader = () => {
-    return (
-        <div style={Top}>
-        <div> 
-            <motion.div
-            style={loadingContainer}
-            variants={loadingContainerVariants}
-            initial="start"
-            animate="end"
-            >
-            <motion.span
-                style={loadingCircle}
-                variants={loadingCircleVariants}
-                transition={loadingCircleTransition}
-            ></motion.span>
+  return (
+    <div style={Top}>
+      <div>
+        <motion.div
+          style={loadingContainer}
+          variants={loadingContainerVariants}
+          initial="start"
+          animate="end"
+        >
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          ></motion.span>
 
-            <motion.span
-                style={loadingCircle}
-                variants={loadingCircleVariants}
-                transition={loadingCircleTransition}
-            ></motion.span>
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          ></motion.span>
 
-            <motion.span
-                style={loadingCircle}
-                variants={loadingCircleVariants}
-                transition={loadingCircleTransition}
-            ></motion.span>
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          ></motion.span>
 
-             <motion.span
-                style={loadingCircle}
-                variants={loadingCircleVariants}
-                transition={loadingCircleTransition}
-            ></motion.span>
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          ></motion.span>
 
-             <motion.span
-                style={loadingCircle}
-                variants={loadingCircleVariants}
-                transition={loadingCircleTransition}
-            ></motion.span>
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          ></motion.span>
         </motion.div>
       </div>
     </div>
